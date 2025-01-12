a3433d9022695758f63feb8290aa8c00da6a23ac
me-safwan-07 <mesafwan07@gmail.com>
2025-01-12 12:59:43 +0530

    Refactor server setup to use ES6 imports and integrate menu routing

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
-const express = require('express');
+import express from 'express';
+import db from './src/utils/db.js';
+import menuRouter from './src/routes/menuRoutes.js';
 
 const app = express();
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, world')
 });
+
+app.use('/api/menu', menuRouter);
 
 app.listen(5000, () => {
     console.log('Server running on port 5000');
