d541e1fc1a5a9c230ac291e13cb05a4c1ed917b8
me-safwan-07 <mesafwan07@gmail.com>
2025-01-13 23:37:51 +0530

    Refactor database connection to use environment variable and async/await pattern

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -1,23 +1,15 @@
 import mongoose from "mongoose";
 
-const db = mongoose.connect("mongodb://localhost:27017/mydatabase")
-   .then(() => {
-        console.log("Connected to MongoDB successfully!");
-    })
-   .catch(error => {
-        console.error("Error connecting to MongoDB:", error.message);
+const connectDB = async () => {
+    try {
+       await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.log("Database connection error: ", error.message);
+        process.exit(1);
+    }
+} 
+    
 
-        // Handle specific error conditions
-        if (error.name === "MongoNetworkError") {
-            console.error("MongoDB connection failed. Please make sure MongoDB is running on localhost:27017.");
-        } else if (error.name === "MongooseServerSelectionError") {
-            console.error("MongoDB connection failed. Please check the connection string and ensure it's correct.");
-        } else {
-            // Handle other types of error
-            console.error("An unexpected error occurred while connecting to MongoDB:", error.message);
-        }
-    });
-
-
-export default db;
+export default connectDB;
 
