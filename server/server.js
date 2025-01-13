1ab1faeb225c498a0c84179c6d068e5496d5fd16
me-safwan-07 <mesafwan07@gmail.com>
2025-01-14 00:38:23 +0530

    Add BASE_API_URL environment variable for API route configuration

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 // Initialize express app and middleware
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const BASE_API_URL = process.env.BASE_API_URL || '/api/v1';
 // Middleware setup
 app.use(bodyParser.json());
 app.use(express.json());
@@ -27,11 +27,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-
 // Connect to MongoDB database
 connectDB();
-
-
 
 // Test route
 app.get('/', (req, res) => {
@@ -39,13 +36,15 @@ app.get('/', (req, res) => {
 });
 
 // Use routes
-app.use('/api/auth', userRouter);
-
-app.use('/api/menu', authMiddleware, menuRouter);
-app.use('/api/order', orderRouter);
-
+app.use(`${BASE_API_URL}/auth`, userRouter);
+app.use(`${BASE_API_URL}/menu`, authMiddleware, menuRouter);
+app.use(`${BASE_API_URL}/order`, authMiddleware, orderRouter);
+// console.log(`${BASE_API_URL}`)
 // Error handling middleware
-
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('Something broke!');
+// });
 
 app.listen(5000, () => {
     console.log('Server running on port 5000');
