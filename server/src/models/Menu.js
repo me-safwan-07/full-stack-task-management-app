b8422d58b631bc0b5e6a64cc44dda1c0481b9ac1
me-safwan-07 <mesafwan07@gmail.com>
2025-01-14 15:40:59 +0530

    Add image field to Menu schema for enhanced menu item representation

--- server/src/models/Menu.js
+++ server/src/models/Menu.js
@@ -5,6 +5,10 @@ const MenuSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    image: {
+        type: String,
+        required: true
+    },
     category: {
         type: String,
         required: true
