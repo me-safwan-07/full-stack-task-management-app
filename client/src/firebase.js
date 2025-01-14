eafd4e41d9a059595013e19ee8d9dffc39bf9e27
me-safwan-07 <mesafwan07@gmail.com>
2025-01-14 15:42:50 +0530

    Add Firebase configuration and initialization for storage

---
+++ client/src/firebase.js
@@ -0,0 +1,24 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import process from 'process';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_APIKEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const storage = getStorage(app);
+
+export { app, storage };
